db945d6fc7c25887dfb6d67131cc83915913270a
Katriel Chiu <katriel_chiu@msn.com>
2024-05-03 11:43:16 -0700

    added models, defined properties and constraints, and established table relationships

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,6 @@
-// Hold our sensitive connection information
 require('dotenv').config();
-
-// Sequelize is used for ORM database applications
 const Sequelize = require('sequelize');
 
-// Create a connection object
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
