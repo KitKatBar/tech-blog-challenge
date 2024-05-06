2f87d3a94c7c8c8bbea54f0f56b23b0b1fd27d6a
Katriel Chiu <katriel_chiu@msn.com>
2024-05-06 03:40:26 -0700

    fixed variable name that was falsely flagging a 500 error

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -55,7 +55,7 @@ router.delete('/:id', withAuth, async (req, res) => {
         return;
     }
   
-        res.status(200).json(postData);
+        res.status(200).json(commentData);
     } catch (err) {
         res.status(500).json(err);
     }
