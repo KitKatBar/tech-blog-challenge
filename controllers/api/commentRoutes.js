40065b2b12d267786fab1a5e3fd8b401dbd0cfad
Katriel Chiu <katriel_chiu@msn.com>
2024-05-07 03:52:39 -0700

    updated input to text area for larger text fields and adjusted some routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -9,7 +9,7 @@ router.post('/', withAuth, async (req, res) => {
         user_id: req.session.user_id,
         post_id: req.body.postId
     });
-  
+        console.log(newComment);
         res.status(200).json(newComment);
     } catch (err) {
         res.status(400).json(err);
