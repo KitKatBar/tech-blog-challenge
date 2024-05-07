ae81bfba6b304f102cf06d0cba74513f1e5e6e93
Katriel Chiu <katriel_chiu@msn.com>
2024-05-07 02:07:37 -0700

    trying to fix configuration

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,10 @@ const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
-      dialect: 'postgres'
+      dialect: 'postgres',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
     });
 
 module.exports = sequelize;
