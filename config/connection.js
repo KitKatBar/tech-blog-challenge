d8d3318b24377ffd053fd696a6782609238701d4
Katriel Chiu <katriel_chiu@msn.com>
2024-05-06 23:34:43 -0700

    changed host configuration variable to process based on the environment it is on

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
+      host: process.env.DB_HOST,
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
